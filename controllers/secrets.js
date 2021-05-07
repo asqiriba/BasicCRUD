422f152ef0a826adad24317a7c82bca552bd053b
asqiriba <federiz@msn.com>
2021-05-06 19:52:14 -0700

    implement DetailsScreen.js features.

---
+++ controllers/secrets.js
@@ -0,0 +1,9 @@
+
+// FireBase credentials.
+export const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+export const authDomain = 'reactnativefirebase-00000.firebaseapp.com';
+export const databaseURL = 'https://reactnativefirebase-00000.firebaseio.com';
+export const projectId = 'reactnativefirebase-00000';
+export const storageBucket = 'reactnativefirebase-00000.appspot.com';
+export const messagingSenderId = '000000000000000';
+export const appId = '1:000000000000000:web:000000000000000';
