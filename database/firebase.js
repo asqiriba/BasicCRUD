396d4eaed5aaaf5c28a70971900d417c790db47e
asqiriba <federiz@msn.com>
2021-05-06 20:11:44 -0700

    refactor firebase.js.

--- database/firebase.js
+++ database/firebase.js
@@ -4,15 +4,17 @@ import firestore from 'firebase/firestore'
 // Import controllers/secrets.js
 import * as secret from '../controllers/secrets'
 
-const firebaseConfig = {
-    apiKey: secret.apiKey,
-    authDomain: secret.authDomain,
-    databaseURL: secret.databaseURL,
-    projectId: secret.projectId,
-    storageBucket: secret.storageBucket,
-    messagingSenderId: secret.messagingSenderId,
-    appId: secret.appId
-};
+// const firebaseConfig = {
+//     apiKey: secret.apiKey,
+//     authDomain: secret.authDomain,
+//     databaseURL: secret.databaseURL,
+//     projectId: secret.projectId,
+//     storageBucket: secret.storageBucket,
+//     messagingSenderId: secret.messagingSenderId,
+//     appId: secret.appId
+// };
+
+const firebaseConfig = secret.firebaseConfig;
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
