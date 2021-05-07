7ae42772f89da30eb2f4e608e3cff5ec533d1cc9
asqiriba <federiz@msn.com>
2021-05-06 18:51:14 -0700

    implement IndexScreen.js functionality.

--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,21 +1,93 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, ActivityIndicator, View } from 'react-native';
+import { ListItem } from 'react-native-elements'
 
-// Import constants.js
+// Import constants.js and database.js.
 import * as constant from '../controllers/constants.js'
+import firebase from '../database/firebase.js';
 
 class IndexScreen extends Component {
+  constructor() {
+    super();
+    this.firestoreRef = firebase.firestore().collection('users');
+    this.state = {
+      isLoading: true,
+      userArr: []
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.firestoreRef.onSnapshot(this.getCollection);
+  }
+
+  // To prevent memory leak.
+  componentWillUnmount(){
+    this.unsubscribe();
+  }
+
+  // Renders the data from the Firebase database and sets the loader to false when fetched.
+  getCollection = (querySnapshot) => {
+    const userArr = [];
+    querySnapshot.forEach((res) => {
+      const { name, email, mobile } = res.data();
+      userArr.push({
+        key: res.id,
+        res,
+        name,
+        email,
+        mobile,
+      });
+    });
+    this.setState({
+      userArr,
+      isLoading: false,
+   });
+  }
+
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicator size = "large" color = {constant.activityIndicatorColor}/>
+        </View>
+      )
+    }    
     return (
-      <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title = "Go to Details Screen"
-          onPress = {() => this.props.navigation.navigate(constant.toDetailsScreen)}
-          color = {constant.buttonColor}
-        />
-    </View>
+      <ScrollView style = {styles.container}>
+          {
+            this.state.userArr.map((item, i) => {
+              return (
+                <ListItem
+                  key = {i}
+                  chevron
+                  bottomDivider
+                  title = {item.name}
+                  subtitle = {item.email}
+                  onPress = {() => {
+                    this.props.navigation.navigate(constant.toDetailsScreen, { userkey: item.key });
+                  }}/>
+              );
+            })
+          }
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingBottom: 22
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default IndexScreen;
